4ed2038b4023b5cfbdaee13cd392261509833756
Katelyn Junker <kjunker@villanova.edu>
2018-10-17 00:50:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,3 @@ let currentUser='defined'
 let welcomeMessage='Welcome to Flatbook,  ' + currentUser +'!'
 let excitedWelcomeMessage= 'WELCOME TO FLATBOOK,  '+ currentUser.toUpperCase() +'!'
 let shortGreeting= 'Welcome, '+ currentUser.charAt(0)
-// .charAt gets you a certain variable = character at (0,1,2, wherever the character is located)
